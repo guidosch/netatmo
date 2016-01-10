182eaf5f4af47fffc457cf47df956b356115ce44
Guido Schnider <guido.schnider@gmail.com>
2016-01-10 23:25:09 +0100

    working example for netatmo access and serve localy

---
+++ netatmo.js
@@ -0,0 +1,71 @@
+var auth = require('./myNetatmoAuth.js');
+var netatmo = require('netatmo');
+var http = require('http');
+const PORT = 8000;
+const HEADERS = {
+  'Content-Type': 'application/json',
+  'Cache-Control': 'no-cache, no-store, must-revalidate',
+  'Pragma': 'no-cache',
+  'Expires': '0'
+}
+
+debugger;
+
+var api = new netatmo(auth());
+
+
+// Get Devicelist 
+// See docs: http://dev.netatmo.com/doc/restapi/devicelist 
+api.getDevicelist(function(err, devices, modules) {
+  //console.log(devices);
+  //console.log(modules);
+});
+
+// Get Measure 
+// See docs: http://dev.netatmo.com/doc/restapi/getmeasure 
+var options = {
+  device_id: '70:ee:50:01:97:20',
+  scale: '30min',
+  type: ['Temperature', 'CO2', 'Humidity'],
+  date_end: 'last',
+  optimize: true
+};
+
+var result = {};
+
+api.getMeasure(options, function(err, measure) {
+
+  /**
+  measure object looks like this:
+  { beg_time: 1452028500, value: [ [ 21.7, 1532, 61 ] ] }
+  **/
+  result.temperature = measure[0].value[0][0];
+  result.co2 = measure[0].value[0][1];
+  result.humidity = measure[0].value[0][2];
+
+});
+
+function handleRequest(request, response) {
+
+  api.getMeasure(options, function(err, measure) {
+
+    /**
+    measure object looks like this:
+    { beg_time: 1452028500, value: [ [ 21.7, 1532, 61 ] ] }
+    **/
+    result.temperature = measure[0].value[0][0];
+    result.co2 = measure[0].value[0][1];
+    result.humidity = measure[0].value[0][2];
+
+  });
+
+  response.writeHead(200, HEADERS);
+  response.end(JSON.stringify(result));
+}
+
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:%s", PORT);
+});
